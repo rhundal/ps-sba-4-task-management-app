243c058e40b0f607fcacabcc8d3dcca9ff0b2577
Rajdeep Hundal <25684974+rhundal@users.noreply.github.com>
2026-09-18 21:42:27 -0500

    added support for unique_id and refactored code

--- script.js
+++ script.js
@@ -25,8 +25,10 @@ Completed
 */
 
 const AVAILABLE_CATEGORIES = ["Work", "Personal", "School", "Errands"];
+let id_counter = 1;
 
 let task = {
+  id: null,
   name: "",
   category: "",
   deadline: new Date("2026-12-31"),
@@ -39,10 +41,6 @@ let upComingTasks,
   completedTasks,
   allTasks = [];
 
-// function constructTask(name, category, date, status) {
-
-// }
-
 function addTask(taskObj) {
   allTasks.push(taskObj);
 }
@@ -52,7 +50,7 @@ function addTask(taskObj) {
 let taskName = document.getElementById("taskName");
 let categoryName = document.getElementById("categoryName");
 let dueDate = document.getElementById("taskDueDate");
-let initialStatus = document.getElementById("statusCheckBoxes");
+let initialStatus = document.getElementById("statusDropDown");
 
 const addTaskBtn = document.getElementById("addTask");
 const taskListArea = document.getElementById("taskList");
@@ -62,18 +60,44 @@ let inprogressListSection = taskListArea.querySelector("#inprogressList");
 let overdueListSection = taskListArea.querySelector("#overdueList");
 let completedListSection = taskListArea.querySelector("#completedList");
 
+function constructTaskObj(nameEl, categoryEl, dueDateEl, statusEl) {
+  let newTask = { ...task }; // 1. Make a fresh, separate copy of the template first
+
+  newTask.id = id_counter++;
+  newTask.name = nameEl.value;
+  newTask.category = categoryEl.value;
+  newTask.deadline = new Date(dueDateEl.value);
+  newTask.status = statusEl.value;
+
+  // 3. Return the independent copy
+  return newTask;
+}
+
 addTaskBtn.addEventListener("click", () => {
-  console.log("task caught!");
+  //   console.log("task caught!");
+  //   console.log(taskNameInput);
+  //   console.log(categoryNameInput);
+  //   console.log(dueDateSelected);
+  //   console.log(statusSelected);
 
-  // construct the task object
+  // construct a list item
+  // add it to the taskList
+  // give crud operations to the list item
 
-  let taskNameInput = taskName.value;
-  let categoryNameInput = categoryName.value;
-  let dueDateSelected = dueDate.value;
-  let statusSelected = initialStatus.value;
+  let newTaskCreated = constructTaskObj(
+    taskName,
+    categoryName,
+    dueDate,
+    initialStatus,
+  );
 
-  console.log(taskNameInput);
-  console.log(categoryNameInput);
-  console.log(dueDateSelected);
-  console.log(statusSelected);
+  allTasks.push(newTaskCreated);
+
+  // Clear inputs after adding
+  taskName.value = "";
+  categoryName.value = "";
+  dueDate.value = "";
+  initialStatus.selectedIndex = 0;
+
+  allTasks.forEach((task) => console.log(task));
 });
